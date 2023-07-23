29ce7313e86dbbeabaccacc91497f71230d36d73
unknown <olivertrain1996@gmail.com>
2023-07-23 12:02:09 +0100

    Added example grid

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    screens: {
+      xs: '552px',
+      sm: '768px',
+      md: '1024px',
+      lg: '1200px',
+      xl: '1440px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
